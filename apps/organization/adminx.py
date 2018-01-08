ae7fa2efb5bcb6753346f085a5b4ed69251f4194
baishuyou <bsy370882@163.com>
2018-01-08 10:05:43 +0800

    Initial commit

---
+++ apps/organization/adminx.py
@@ -0,0 +1,26 @@
+# _*_ encoding:utf-8 _*_
+_author_ = 'baisy'
+_date_ = '2018/1/7 15:17'
+
+import xadmin
+
+from .models import CityDict, CourseOrg, Teacher
+
+class CityDictAdmin(object):
+    list_display = ['name', 'desc', 'add_time']
+    search_fields = ['name', 'desc']
+    list_filter = ['name', 'desc', 'add_time']
+
+class CourseOrgAdmin(object):
+    list_display = ['name', 'desc', 'click_nums', 'fav_nums']
+    search_fields = ['name', 'desc', 'click_nums', 'fav_nums']
+    list_filter = ['name', 'desc', 'click_nums', 'fav_nums']
+
+class TeacherAdmin(object):
+    list_display = ['org', 'name', 'work_years', 'work_company']
+    search_fields = ['org', 'name', 'work_years', 'work_company']
+    list_filter = ['org', 'name', 'work_years', 'work_company']
+
+xadmin.site.register(CityDict, CityDictAdmin)
+xadmin.site.register(CourseOrg, CourseOrgAdmin)
+xadmin.site.register(Teacher, TeacherAdmin)
